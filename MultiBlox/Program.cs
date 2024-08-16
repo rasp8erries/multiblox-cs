bde0cabd17f41bc07d5766b5e49e31b7fb0caea7
qwqtoday <lukas.leung@hotmail.com>
2024-08-16 12:54:15 +0800

    Use 2024 C# features.

--- MultiBlox/Program.cs
+++ MultiBlox/Program.cs
@@ -49,7 +49,7 @@ namespace MultiBlox
                 }
                 catch(Exception ex)
                 {
-                    Console.WriteLine("ERROR: {0}",ex.Message); // smth wtf happen lol
+                    Console.WriteLine($"ERROR: {ex.Message}"); // smth wtf happen lol
                 }
                 
             }
@@ -62,7 +62,7 @@ namespace MultiBlox
                 }
                 else
                 {
-                    Console.WriteLine("ERROR: Unexpected number of output lines! Output was: \r\n{0}",output); // otherwise smth is very wrong..
+                    Console.WriteLine($"ERROR: Unexpected number of output lines! Output was: \r\n{output}"); // otherwise smth is very wrong..
                     return null;
                 }
             }
@@ -77,17 +77,11 @@ namespace MultiBlox
         /// Searches for all the running Roblox instances & returns array of the process IDs.
         /// </summary>
         /// <returns>string array of roblox PIDs</returns>
-        static string[] FindRobloxProcesses()
-        {
-            // get roblox processes ordered by longest running first
-            var robloxProcs = Process.GetProcessesByName("RobloxPlayerBeta").OrderByDescending(x => DateTime.Now - x.StartTime).ToArray();
-            string[] pids = new string[robloxProcs.Length];
-            for(var i = 0; i < robloxProcs.Length; i++)
-            {
-                pids[i] = robloxProcs[i].Id.ToString();
-            }
-            return pids;
-        }
+        static string[] FindRobloxProcesses() =>
+             Process.GetProcessesByName("RobloxPlayerBeta")
+            .OrderByDescending(x => DateTime.Now - x.StartTime)
+            .Select(robloxProc => robloxProc.Id.ToString())
+            .ToArray();
 
         /// <summary>
         /// Searches for a Roblox Event Handle for a specified process ID.
@@ -99,11 +93,11 @@ namespace MultiBlox
         static bool FindAndCloseRobloxEvent(string pid, string handleName)
         {
             bool foundAndClosed = false;
-            Console.WriteLine("[PID:{0}] Searching for {1}...",pid,handleName);
+            Console.WriteLine($"[PID:{pid}] Searching for {handleName}...");
             // call sysinternals to search for the roblox event handle 
             // see here for info on the args etc: 
             // https://learn.microsoft.com/en-us/sysinternals/downloads/handle#usage
-            string[]? outlines = RunHandleProc(string.Format("-a -p {0} {1} -v -accepteula",pid,handleName)); // adding 'accepteula' arg bc it will fail if its never been accepted 
+            string[]? outlines = RunHandleProc($"-a -p {pid} {handleName} -v -accepteula"); // adding 'accepteula' arg bc it will fail if its never been accepted 
             if (outlines!=null) // if its null we got an error 
             {
                 if (outlines.Length>=1 && outlines[0].Trim()=="No matching handles found.")
@@ -118,33 +112,33 @@ namespace MultiBlox
                         var _pid = handlevalues[1]; // process ID
                         var hid = handlevalues[3]; // handle ID
 
-                        Console.WriteLine("[PID:{0}] {1} found! Closing...",_pid,handleName);
+                        Console.WriteLine($"[PID:{_pid}] {handleName} found! Closing...");
                         
                         // call sysinternals using -c arg to specify a handle to close
                         // the -y arg bypasses confirmation before closing handle 
                         // and the -p arg is where we're specifying which process ID 
-                        outlines = RunHandleProc(string.Format("-c {0} -y -p {1}",hid,_pid));
+                        outlines = RunHandleProc($"-c {hid} -y -p {pid}");
                         if (outlines!=null)
                         {
                             if (outlines.Length>=3 && outlines[2].Trim()=="Handle closed.") // confirming output so we know that it worked 
                             {
-                                Console.WriteLine("[PID:{0}] {1} closed!",_pid,handleName);
+                                Console.WriteLine($"[PID:{_pid}] {handleName} closed!");
                                 foundAndClosed = true;
                             }
                             else // otherwise smth must have gone wrong..
                             {
-                                Console.WriteLine("[PID:{0}] ERROR! Unexpected output! Output was: \r\n{1}",_pid,string.Join('\n',outlines));
+                                Console.WriteLine($"[PID:{_pid}] ERROR! Unexpected output! Output was: \r\n{string.Join('\n', outlines)}");
                             }
                         }
                     }
                     else
                     {
-                        Console.WriteLine("[PID:{0}] ERROR! Unexpected number of handle values! Values were: \r\n{1}",pid,outlines[1].Trim());
+                        Console.WriteLine($"[PID:{pid}] ERROR! Unexpected number of handle values! Values were: \r\n{outlines[1].Trim()}");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("[PID:{0}] ERROR! Unexpected output! Output was: \r\n{1}",pid,string.Join('\n',outlines));
+                    Console.WriteLine($"[PID:{pid}] ERROR! Unexpected output! Output was: \r\n{string.Join('\n', outlines)}");
                 }
             }
             else
@@ -169,7 +163,7 @@ namespace MultiBlox
             if (pid!=null)
             {
                 Console.WriteLine("");
-                Console.WriteLine("New Roblox Instance Detected! PID:{0}",pid);
+                Console.WriteLine($"New Roblox Instance Detected! PID:{pid}");
                 var evQueue = new Queue<string>(_ROBLOX_EVENTS); // make a new queue from the static event handle names
                 string? currEv = null;
                 while (evQueue.Count>0) // loop while the queue has anything in it 
@@ -225,7 +219,7 @@ namespace MultiBlox
             
             if (pids.Length>0)
             {
-                Console.WriteLine("Found {0} Roblox instance(s) running. PIDS: {1}",pids.Length,string.Join(", ",pids));
+                Console.WriteLine($"Found {pids.Length} Roblox instance(s) running. PIDS: {string.Join(", ", pids)}");
 
                 var results = new List<Tuple<string,string,bool>>();
                 foreach(var pid in pids)
@@ -244,7 +238,7 @@ namespace MultiBlox
                 bool foundAny = false;
                 foreach(var tpl in results)
                 {
-                    Console.WriteLine("[PID:{0}] {1} {2}",tpl.Item1,tpl.Item2,tpl.Item3 ? "CLOSED" : "NOT FOUND");
+                    Console.WriteLine($"[PID:{tpl.Item1}] {tpl.Item2} {(tpl.Item3 ? "CLOSED" : "NOT FOUND")}");
                     if (tpl.Item3)
                         foundAny = true;
                 }
